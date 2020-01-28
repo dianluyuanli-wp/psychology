0624f19c197821f670381f0ac18605ce94f816f3
xiaohuli <waterdoor@foxmail.com>
2020-01-28 21:44:36 +0800

    fix: 小程序微调

--- miniprogram/pages/detail/detail.ts
+++ miniprogram/pages/detail/detail.ts
@@ -8,8 +8,7 @@ Page({
     data: {
         counselor: '',
         heighLightIndex: 1000,
-        timeList: [{date: '1月3号', time: '五点到十点', periodId: '0'}, {date: '1月4号', time: '五点到十点', periodId: '1'},
-        {date: '1月5号', time: '五点到十点', periodId: '2'}],
+        timeList: [{ date: '', time: '', _id: '', counselorId: ''}],
 
         userInfo: {},
         hasUserInfo: false,
@@ -33,21 +32,6 @@ Page({
           }
         ]
     },
-    onShow() {
-        let pages = getCurrentPages();
-        let currPage = null;
-        // console.log(pages) 的到一个数组
-        if (pages.length) {
-          // 获取当前页面的对象（上边所获得的数组中最后一项就是当前页面的对象）
-          currPage = pages[pages.length - 1];
-        }
-        // 获取当前页面的路由
-        let route = currPage?.options.name;
-        this.setData({
-            counselor: route
-        });
-        console.log(route);
-    },
     check(event: DomEvent) {
         this.setData({
             heighLightIndex: event.currentTarget.dataset.index
@@ -59,33 +43,33 @@ Page({
       url: '../logs/logs',
     })
   },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true,
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true,
-          })
-        },
-      })
+  async onLoad() {
+    let pages = getCurrentPages();
+    let currPage = null;
+    // console.log(pages) 的到一个数组
+    if (pages.length) {
+      // 获取当前页面的对象（上边所获得的数组中最后一项就是当前页面的对象）
+      currPage = pages[pages.length - 1];
     }
+    // 获取当前页面的路由
+    let route = currPage?.options.name;
+    this.setData({
+        counselor: route
+    });
+    const res = await db.collection('period').where({
+      counselorId: route
+    }).get();
+    this.setData({
+      timeList: res.data.map(item => {
+        const { date, startTime, endTime, counselorId } = item;
+        return {
+          date,
+          time: startTime + '--' + endTime,
+          counselorId
+        }
+      }) as Array<any>
+    });
+    console.log(res);
   },
   getUserInfo(e: any) {
     app.globalData.userInfo = e.detail.userInfo
@@ -141,11 +125,11 @@ Page({
               const defaultInfo = {
                 saySome: 'nothing'
               };
-              const { date, time, periodId } = this.data.timeList[this.data.heighLightIndex];
+              const { date, time, _id, counselorId } = this.data.timeList[this.data.heighLightIndex];
               //    避免重复预约
               const res = await db.collection('interviewee').where({
                 openId: app.globalData.openId,
-                periodId: periodId
+                _id
               }).count();
               if (res.total) {
                 this.setData({
@@ -156,12 +140,12 @@ Page({
               await db.collection('interviewee').add({
                 data: {
                   formData: Object.assign(defaultInfo, this.data.formData, { date, time }),
-                  userInfo: this.data.userInfo,
+                  userInfo: app.globalData.userInfo,
                   openId: app.globalData.openId,
                   status: 'apply',
                   counselorName: this.data.counselor,
-                  counselorId: '0001',
-                  periodId: periodId
+                  counselorId: counselorId,
+                  periodId: _id
                 }
               });
               wx.showToast({
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,3 +1,9 @@
+const app = getApp<IAppOption>();
+
+// const db = wx.cloud.database({
+//   env: 'test-psy-qktuk'
+// });
+
 Page({
     // onShareAppMessage() {
     //   return {
@@ -7,6 +13,7 @@ Page({
     // },
   
     data: {
+      canIUse: wx.canIUse('button.open-type.getUserInfo'),
       background: [{ src: 'http://cms-bucket.ws.126.net/2020/0123/564125bej00q4jo8e002tc000go008cc.jpg?imageView&thumbnail=600y300'},
         { src: 'http://cms-bucket.ws.126.net/2020/0119/f543f553j00q4cbge002uc000go008cc.jpg?imageView&thumbnail=600y300'}, {
           src: 'http://cms-bucket.ws.126.net/2020/0123/f25d19daj00q4ji4k001uc000go008cc.jpg?imageView&thumbnail=600y300'
@@ -25,6 +32,35 @@ Page({
       autoplay: false,
       interval: 2000,
       duration: 500
+    },
+
+    onLoad() {
+      if (app.globalData.userInfo) {
+        this.setData({
+          userInfo: app.globalData.userInfo,
+          hasUserInfo: true,
+        })
+      } else if (this.data.canIUse) {
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        app.userInfoReadyCallback = res => {
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+          })
+        }
+      } else {
+        // 在没有 open-type=getUserInfo 版本的兼容处理
+        wx.getUserInfo({
+          success: res => {
+            app.globalData.userInfo = res.userInfo
+            this.setData({
+              userInfo: res.userInfo,
+              hasUserInfo: true,
+            })
+          },
+        })
+      }
     },
 
     content(event: DomEvent) {
@@ -57,3 +93,5 @@ Page({
       })
     }
   })
+
+  export default {};
